551969b81aba0fbe091e6437845b9da9d91f2d5d
David Wheeler <davidwheeler@wless-user-172019182162.temp.wsu.edu>
2020-09-23 11:25:33 -0700

    adding data figs and scripts

---
+++ SCRIPTS/.ipynb_checkpoints/EEPS-checkpoint.R
@@ -0,0 +1,471 @@
+#-----------------------------------------------------------------------#
+# VERTICILLIUM IS ATTENUATED IN AGGRESSIVENESS AFTER SERIAL PASSAGE 
+# THROUGH SPECIFIC HOSTS 
+# DAVID LINNARD WHEELER
+# 1/18/2015
+#-----------------------------------------------------------------------#
+
+# META DATA OUTLINE:
+  # HYPOTHESES  
+  # EXPERIMENTAL DESIGN
+  # OPEN AND CURATE DATA
+  # SUMMARY STATISTICS
+  # EXPLORATORY DATA ANALYSES
+  # DIAGNOSTICS
+  # PARAMETRIC ANALYSES
+  # MANUSCRIPT
+#-----------------------------------------------------------------------#
+
+# HYPOTHESES
+  # SCIENCE Ho: AGGRESSIVENESS AND FITNESS OF VERTICILLIUM DAHLIAE TO POTATO 
+              # IS NOT ATTENUATED AFTER SERIAL PASSAGE THROUGH HOSTS
+  # SCIENCE Ho: VEGETATIVE COMPATIBILITY GROUPS ARE STABLE AFTER SERIAL PASSAGE
+              # THROUGH HOSTS
+  # SCIENCE Ho: ASYMPTOMATIC INFECTIONS ARE STABLE AFTER SERIAL PASSAGE 
+              # THROUGH HOSTS
+  # SCIENCE Ho: COINFECTION OF HOSTS BY VERTICILLIUM DAHLIAE ISOLATES OF
+              # BOTH MATING TYPES DOES NOT AFFECT (i) DISEASE, 
+              # (ii) INOCULUM DENSITY, OR (iii) RESULT IN TELEOMORPH FORMATION
+  
+  # STATISTICAL Ho: Yijk=u+Eijk vs. Ha: Yijk=u+Ai+Bj+Ck+EiJk
+    # Observed variation in response variables represent random variation, not 
+    #   systematic effects of hosts and isolates. Variation in the response variable is 
+    #   unrelated to variation in the predictor variable and is no greater than expected 
+    #   by chance or sampling error.
+#-----------------------------------------------------------------------#
+# EXPERIMENTAL DESIGN: G1,G2,G3,G4,G5
+# TREATMENT STRUCTURE: 2 WAY FACTORIAL DESIGN
+# INDEPENDENT VARIABLES: 
+  # 3 HOSTS(POTATO, BROWN MUSTARD ISCI 99, BARLEY 'BARONESS') x 
+  # 4 ISOLATES(653(4A),111(2B),461(2), CONTROL) x
+  # 10 REPLICATES x 
+  # 2 TRIALS
+# DEPENDENT VARIABLES: 
+  # IN PLANTA: 
+    # DISEASE: CHLOROSIS,NECROSIS;INCIDENCE OF INFECTED STEMS,CFU/g OF STEMS
+    # BIOMASS: STEM & ROOT HEIGHT & WEIGHT;TUBER WEIGHT,TUBER NUMBER
+  # IN FUNGUS: 
+    # FITNESS: RADIAL GROWTH; CONIDIA, MICROSCLEROTIA SIZE & NUMBER
+    # ANASTOMOSIS: VCG
+    # GENOME: SNPs OR SSRs
+
+# DESIGN STRUCTURE: RANDOMIZED COMPLETE BLOCK DESIGN
+# EXPERIMENTAL UNIT: PLANTS 
+# OBSERVATIONAL UNIT: PLANTS
+# SAMPLES: STEMS, ROOTS, SOIL
+# NOTATION: HOST-ISOLATE-REPLICATE-TRIAL
+  # HOSTS:
+    # POTATO = 1
+    # MUSTARD = 2
+    # BARLEY = 3
+  # ISOLATES
+    # 653 @ G1 = 1.1; @ G2 = 1.2...etc
+    # 111 @ G1 = 2.1; @ G2 = 2.2...etc
+    # 461 @ G1 = 3.1; @ G2 = 3.2...etc
+    # NON-INOCULATED CONTROL = 4
+
+# DATA: AUSPS, AUSPC, YIELD, BIOMASS, INCIDENCE OF INFECTED STEMS & ROOTS, STEM & ROOT INOCULUM DENSITY,
+      # RADIAL GROWTH, CONIDIA & MICROSCLEROTIA DIMENSIONS & NUMBER, VCGs, MOLECULAR MARKER
+
+# ANALYSES: 
+# ANOVA OF AUSPS, AUSPC, YIELD, INCIDENCE OF INFECTED STEMS, INOCULUM DENSITY, FUNGAL FITNESS
+#-----------------------------------------------------------------------#
+# MATERIALS AND METHODS: G1
+    # INOCULUM PREPARATION FOR SOIL DRENCH (3.5"pot):
+      # 10^5 = 100000 CFU/cm^3 x 500 cm^3/1 pot = 50000000 CFU/Pot
+      # 50000000 CFU/Pot x ml/10^6 CFU = 50 ml/Pot
+    # ALL ISOLATES
+      # INOCULATE WITH 50 ml/Pot OF 10^6 CFU/ml
+    # TOTAL INOCULUM
+      # (ALL ISOLATES) 
+      # 50 ml/Pot * 3 HOSTS * 10 REPLICATES * 2 TRIALS = 3000 ml/ISOLATE OR 1500 ml/ISOLATE/TRIAL
+      # EACH ISOLATE
+      # 1500ml/200ml PER CULTURE = 7.5 CULTURES/ISOLATE/TRIAL
+# MATERIALS AND METHODS: G2,G3,G4,G5
+    # INOCULUM PREPARATION FOR SOIL DRENCH (3.5"pot):
+      # 10^5 = 100000 CFU/cm^3 x 500 cm^3/1 pot = 50000000 CFU/Pot
+      # 50000000 CFU/Pot x ml/10^6 CFU = 50 ml/Pot
+    # ALL ISOLATES
+      # INOCULATE WITH 50 ml/Pot OF 10^6 CFU/ml
+    # TOTAL INOCULUM
+      # (ALL ISOLATES) 
+      # 50 ml/Pot * 1 HOSTS * 10 REPLICATES * 2 TRIALS = 1000 ml/ISOLATE OR 500 ml/ISOLATE/TRIAL
+      # EACH ISOLATE
+      # 500ml/200ml PER CULTURE = 2.5 = 3 CULTURES/ISOLATE/TRIAL OR 5 CULTURES/ISOLATE
+#-----------------------------------------------------------------------#
+# PLANTING AND INOCULATION DATES
+    # TRIAL T1 ONE PLANTED: 7/7/2017
+    # TRIAL T1 ONE INOCULATED: 8/11/2017 
+      # OBSERVED V.dahliae ON SAMPLED STEMS ON POTATO AND BRASSICA FROM 8/28
+    # TRIAL T1 TWO PLANTED: 7/14/2017
+    # TRIAL T1 TWO INOCULATED: 8/18/2017
+
+    # TRIAL T2 ONE PLANTED: 12/13/2018
+    # TRIAL T2 ONE INOCULATED: 1/22/2018 
+    # TRIAL T2 TWO PLANTED: 12/13/2018
+    # TRIAL T2 TWO INOCULATED: 1/22/2018
+
+    # TRIAL T3 ONE PLANTED: 4/2/2018
+    # TRIAL T3 ONE INOCULATED: 4/24/2018 
+    # TRIAL T3 ONE HARVESTED: 5/9/2018 
+    # TRIAL T3 TWO PLANTED: 4/2/2018
+    # TRIAL T3 TWO INOCULATED: 4/24/2018
+    # TRIAL T3 TWO HARVESTED: 5/9/2018 
+    
+    # TRIAL T4 ONE PLANTED: 6/6/2018
+    # TRIAL T4 ONE INOCULATED: 6/29/2018 
+    # TRIAL T4 TWO PLANTED: 6/6/2018
+    # TRIAL T4 TWO INOCULATED:6/29/2018
+
+    # TRIAL B1 ONE PLANTED: //2018
+    # TRIAL B1 ONE INOCULATED: //2018 
+    # TRIAL B1 TWO PLANTED: //2018
+    # TRIAL B1 TWO INOCULATED: //2018
+
+#-----------------------------------------------------------------------#
+#-----------------------------------------------------------------------#
+# EXPERIMENTAL DESIGN: BIOASSAY
+# TREATMENT STRUCTURE: 2 WAY FACTORIAL DESIGN
+# INDEPENDENT VARIABLES: 
+  # 1 HOST(POTATO) x 
+  # 37 ISOLATES (653(4A),111(2B),461(2)+NON-INOCULATED CONTROL)
+    # 653(4A),111(2B), OR 461(2) FROM POTATO @ T1
+    # 653(4A),111(2B), OR 461(2) FROM MUSTARD @ T1
+    # 653(4A),111(2B), OR 461(2) FROM BARLEY @ T1
+    # 653(4A),111(2B), OR 461(2) FROM POTATO @ T2
+    # 653(4A),111(2B), OR 461(2) FROM MUSTARD @ T2
+    # 653(4A),111(2B), OR 461(2) FROM BARLEY @ T2
+    # 653(4A),111(2B), OR 461(2) FROM POTATO @ T3
+    # 653(4A),111(2B), OR 461(2) FROM MUSTARD @ T3
+    # 653(4A),111(2B), OR 461(2) FROM BARLEY @ T3
+    # 653(4A),111(2B), OR 461(2) FROM POTATO @ T4
+    # 653(4A),111(2B), OR 461(2) FROM MUSTARD @ T4
+    # 653(4A),111(2B), OR 461(2) FROM BARLEY @ T4
+  # 5 REPLICATES x 
+  # 2 TRIALS
+# DEPENDENT VARIABLES: 
+# IN PLANTA: 
+# DISEASE: CHLOROSIS,NECROSIS;INCIDENCE OF INFECTED STEMS,CFU/g OF STEMS
+# BIOMASS: STEM & ROOT HEIGHT & WEIGHT;TUBER WEIGHT,TUBER NUMBER
+# IN FUNGUS: 
+# FITNESS: RADIAL GROWTH; CONIDIA, MICROSCLEROTIA SIZE & NUMBER
+# ANASTOMOSIS: VCG
+# GENOME: SNPs OR SSRs
+
+# DESIGN STRUCTURE: RANDOMIZED COMPLETE BLOCK DESIGN
+# EXPERIMENTAL UNIT: PLANTS 
+# OBSERVATIONAL UNIT: PLANTS
+# SAMPLES: STEMS, ROOTS, SOIL
+# DATA: AUSPS, AUSPC, YIELD, BIOMASS, INCIDENCE OF INFECTED STEMS & ROOTS, STEM & ROOT INOCULUM DENSITY,
+# RADIAL GROWTH, CONIDIA & MICROSCLEROTIA DIMENSIONS & NUMBER, VCGs, MOLECULAR MARKER
+
+# ANALYSES: 
+# ANOVA OF AUSPS, AUSPC, YIELD, INCIDENCE OF INFECTED STEMS, INOCULUM DENSITY, FUNGAL FITNESS
+
+# QUESTIONS
+  # IS TIME IN PURE CULTURE CONFOUNDING ATTENUATION?
+  # CAN WE RECOVER ONE ISOLATE FROM ONE CROP FOR Tn+1?
+  # WHAT DATA CAN BE COLLECTED?
+#-----------------------------------------------------------------------#
+# MATERIALS AND METHODS: BIOASSAY
+
+# INOCULUM PREPARATION FOR SOIL DRENCH (3.5"pot) WITH 10 REPS:
+  # 10^5 = 100000 CFU/cm^3 x 500 cm^3/1 pot = 50000000 CFU/Pot
+  # 50000000 CFU/Pot x ml/10^6 CFU = 50 ml/Pot
+# ALL ISOLATES
+  # INOCULATE WITH 50 ml/Pot OF 10^6 CFU/ml
+# TOTAL INOCULUM
+# (ALL ISOLATES) 
+  # 50 ml/Pot * 1 HOSTS * 10 REPLICATES * 2 TRIALS = 1000 ml/ISOLATE OR 500 ml/ISOLATE/TRIAL
+# EACH ISOLATE
+  # 500ml/200ml PER CULTURE = 2.5 = 3 CULTURES/ISOLATE/TRIAL OR 5 CULTURES/ISOLATE
+    # 5 CULTURES/ISOLATE * 36 ISOLATES = 180 CULTURES
+    # 3 CULTURES/ISOLATE * 36 ISOLATES = 108 CULTURES
+
+# INOCULUM PREPARATION FOR SOIL DRENCH (3.5"pot) WITH 5 REPS:
+  # 10^5 = 100000 CFU/cm^3 x 500 cm^3/1 pot = 50000000 CFU/Pot
+  # 50000000 CFU/Pot x ml/10^6 CFU = 50 ml/Pot
+# ALL ISOLATES
+  # INOCULATE WITH 50 ml/Pot OF 10^6 CFU/ml
+# TOTAL INOCULUM
+# (ALL ISOLATES) 
+  # 50 ml/Pot * 1 HOSTS * 5 REPLICATES * 2 TRIALS = 500 ml/ISOLATE OR 250 ml/ISOLATE/TRIAL
+# EACH ISOLATE
+  # 250ml/200ml PER CULTURE = 1.5 = 2 CULTURES/ISOLATE/TRIAL OR 4 CULTURES/ISOLATE
+    # 4 CULTURES/ISOLATE * 36 ISOLATES = 144 CULTURES
+    # 2 CULTURES/ISOLATE * 36 ISOLATES = 72 CULTURES
+
+# TOTAL INOCULUM
+# (ALL ISOLATES) 
+# 50 ml/Pot * 1 HOSTS * 10 REPLICATES * 2 TRIALS = 1000 ml/ISOLATE OR 500 ml/ISOLATE/TRIAL
+# EACH ISOLATE
+# 500ml/200ml PER CULTURE = 2.5 = 3 CULTURES/ISOLATE/TRIAL OR 5 CULTURES/ISOLATE
+
+
+# TRIAL ONE PLANTED: 
+# TRIAL ONE INOCULATED:  10/1/2018
+# TRIAL TWO PLANTED: 
+# TRIAL TWO INOCULATED: 11/19/2018
+#-----------------------------------------------------------------------#
+#-----------------------------------------------------------------------#
+# EXPERIMENTAL DESIGN: PILOT
+# TREATMENT STRUCTURE: 1 WAY DESIGN
+# INDEPENDENT VARIABLES: HOSTS(1(POTATO)) x 
+#  ISOLATES(3(VD5VSP699.P,VD5VSP699.B, CONTROL) 
+#  x REPLICATES(6) x TRIALS(2)= 
+# DEPENDENT VARIABLES: CHLOROSIS,NECROSIS,TUBER WEIGHT,TUBER NUMBER,
+# INCIDENCE OF INFECTED STEMS,CFU/g OF STEMS, VCG,
+# TELEOMORPH, RADIAL GROWTH
+# DESIGN STRUCTURE: RANDOMIZED COMPLETE BLOCK DESIGN
+# EXPERIMENTAL UNIT: PLANTS INOCULATED WITH ISOLATES
+# OBSERVATIONAL UNIT: PLANTS
+# SAMPLES: STEMS, ROOTS, SOIL
+# DATA: AUSPS, AUSPC, YIELD, INCIDENCE OF INFECTED STEMS,
+# VERTICILLIUM DAHLIAE CFU/g of stem
+# ANALYSES: 
+# T-TESTS OR ANOVA OF AUSPS, AUSPC, YIELD,INCIDENCE OF INFECTED STEMS,
+# VERTICILLIUM DAHLIAE CFU/g OF STEM
+
+#-----------------------------------------------------------------------#
+# OPEN AND CURATE DATA
+##### EXPERIMENTAL EVOLUTION DATA ANALYSES #####
+### DAVID LINNARD WHEELER ###
+## 11/16/2015 ##
+
+# IMPORT DATA
+EE=read.csv(file.choose(), header=T)
+EE
+str(EE)
+attach(EE)
+
+# COERCE DATA TO PROPER CLASSES
+VD=factor(FUNGUS)
+BLOCKS=factor(BLOCK)
+TRIALS=factor(TRIAL)
+EED=data.frame(VD,BLOCKS,TRIALS,JULY_27,AUG_3,AUG_18,AUG_24,AUSPC)
+attach(EED)
+
+# SUMMARY STATISTICS
+# EXPLORATORY DATA ANALYSES
+# DIAGNOSTICS
+# PARAMETRIC ANALYSES
+# ANOVA OF AUSPC DATA
+ANOVA=lm(SENESCENCE~VD)
+summary(ANOVA)
+anova(ANOVA)
+Anova(ANOVA,type=c(2)) # REQUIRES cars 
+
+ANOVA2=aov(SENESCENCE~VD)
+plot(ANOVA2)
+summary(ANOVA2)
+
+# PAIRWISE COMPARISONS
+EE_HSD=TukeyHSD(ANOVA2)
+plot(EE_HSD)
+TukeyHSD(ANOVA2)
+
+# AUSPS
+library(agricolae)
+AUSPS1=audps(EED[1,4:7],EED[1,4:7])
+AUSPS2=audps(EED[2,4:7],EED[1,4:7])
+AUSPS3=audps(EED[3,4:7],EED[1,4:7])
+AUSPS4=audps(EED[4,4:7],EED[1,4:7])
+AUSPS5=audps(EED[5,4:7],EED[1,4:7])
+AUSPS6=audps(EED[6,4:7],EED[1,4:7])
+AUSPS7=audps(EED[7,4:7],EED[1,4:7])
+AUSPS8=audps(EED[8,4:7],EED[1,4:7])
+AUSPS9=audps(EED[9,4:7],EED[1,4:7])
+AUSPS10=audps(EED[10,4:7],EED[1,4:7])
+AUSPS11=audps(EED[11,4:7],EED[1,4:7])
+AUSPS12=audps(EED[12,4:7],EED[1,4:7])
+AUSPS13=audps(EED[13,4:7],EED[1,4:7])
+AUSPS14=audps(EED[14,4:7],EED[1,4:7])
+AUSPS15=audps(EED[15,4:7],EED[1,4:7])
+AUSPS16=audps(EED[16,4:7],EED[1,4:7])
+AUSPS17=audps(EED[17,4:7],EED[1,4:7])
+AUSPS18=audps(EED[18,4:7],EED[1,4:7])
+AUSPS19=audps(EED[19,4:7],EED[1,4:7])
+AUSPS20=audps(EED[20,4:7],EED[1,4:7])
+AUSPS21=audps(EED[21,4:7],EED[1,4:7])
+AUSPS22=audps(EED[22,4:7],EED[1,4:7])
+AUSPS23=audps(EED[23,4:7],EED[1,4:7])
+AUSPS24=audps(EED[24,4:7],EED[1,4:7])
+AUSPS25=audps(EED[25,4:7],EED[1,4:7])
+AUSPS26=audps(EED[26,4:7],EED[1,4:7])
+AUSPS27=audps(EED[27,4:7],EED[1,4:7])
+AUSPS28=audps(EED[28,4:7],EED[1,4:7])
+AUSPS29=audps(EED[29,4:7],EED[1,4:7])
+AUSPS30=audps(EED[30,4:7],EED[1,4:7])
+AUSPS31=audps(EED[31,4:7],EED[1,4:7])
+AUSPS32=audps(EED[32,4:7],EED[1,4:7])
+AUSPS33=audps(EED[33,4:7],EED[1,4:7])
+AUSPS34=audps(EED[34,4:7],EED[1,4:7])
+AUSPS35=audps(EED[35,4:7],EED[1,4:7])
+AUSPS36=audps(EED[36,4:7],EED[1,4:7])
+EEAUSPS=c(AUSPS1,AUSPS2,AUSPS3,AUSPS4,AUSPS5,AUSPS6,
+          AUSPS7,AUSPS8,AUSPS9,AUSPS10,AUSPS11,AUSPS12,
+          AUSPS13,AUSPS14,AUSPS15,AUSPS16,AUSPS17,AUSPS18,
+          AUSPS19,AUSPS20,AUSPS21,AUSPS22,AUSPS23,AUSPS24,
+          AUSPS25,AUSPS26,AUSPS27,AUSPS28,AUSPS29,AUSPS30,
+          AUSPS31,AUSPS32,AUSPS33,AUSPS34,AUSPS35,AUSPS36)
+SENESCENCE=unlist(EEAUSPS, use.names=F)
+str(SENESCENCE)
+EEDATA=data.frame(VD,BLOCKS,TRIALS,JULY_27,AUG_3,AUG_18,AUG_24,AUSPC,SENESCENCE)
+
+# MANUSCRIPT
+ABSTRACT
+INTRODUCTION
+MATERIALS AND METHODS
+RESULTS
+DISCUSSION
+
+#-----------------------------------------------------------------------#
+# OPEN AND CURATE DATA
+# SET WORKING DIRECTORY
+setwd("C:/Users/david.wheeler/Desktop/UPDATE/EE")
+# INVOKE PACKAGES
+library(ggplot2)
+# IMPORT DATA
+# OPEN AND CURATE DATA: 
+EE=read.csv(file.choose(),header=T)
+str(EE)
+EE[EE=="."]=NA
+TRIALS=as.factor(as.character(EE$TRIAL))
+HOSTS=as.factor(as.character(EE$HOST))
+ISOLATES=as.factor(as.character(EE$ISOLATE))
+BLOCKS=as.factor(as.character(EE$BLOCK))
+YIELDS=as.numeric(as.character(EE$YEILD_g))
+TUBER_N=as.numeric(as.character(EE$TUBER_NO.))
+SENESCENCE=as.numeric(as.character(EE$AUSPC))
+attach(EE)
+EE=data.frame(TRIALS,HOSTS,ISOLATES,BLOCKS,
+                        YIELDS,TUBER_N,SENESCENCE)
+str(EE)           
+attach(EE)
+EE[EE=="."]=NA
+#-----------------------------------------------------------------------#
+#-----------------------------------------------------------------------#
+# EXPLORATORY DATA ANALYSIS
+## BOXPLOTS FOR YIELD
+# CREATE FACTORS FOR EACH FACTOR
+ISOLATE=factor(EE$ISOLATES,levels=c("4","1","2","3"))
+
+HOST=factor(EE$HOST,levels=c("POTATO","MUSTARD","BARLEY"))
+
+YIELDS=ggplot(data=EE,aes(x=ISOLATE,y=YIELDS,group=ISOLATE,colour=HOST)) +
+  geom_boxplot(outlier.size=0,alpha=1/2,lwd=0.72,notch=F)+
+  geom_jitter(aes(fill=HOST),size=8,pch=20,alpha = 1/3,position=position_jitter(w=0.05,h=0.01))+
+  scale_colour_manual(values = c("black","gray26","gray55"))+
+  theme_bw()+theme(panel.grid.major=element_line(colour="white"))+
+  labs(x="Hosts",y="Yield (g)")+
+  theme(axis.title.x = element_text(size=18,face="bold"))+
+  theme(axis.title.y = element_text(size=18,face="bold"))+
+  scale_x_discrete(labels=c("Control","653",
+                            "111","461"))+
+  theme(axis.title.x = element_text(size=10))+
+  theme(axis.title.y = element_text(size=10))+
+  theme(axis.text.x=element_text(angle=1,size=10,face="bold"))+
+  theme(axis.text.y=element_text(size=10,face="bold"))+
+  theme(legend.position="none")+
+  facet_grid(HOSTS~.,scales="free")
+tiff("YIELDS.tiff",width=8,height=5,units='in',res=600) 
+YIELDS
+dev.off()
+
+## BOXPLOTS FOR AUSPC
+# SUBSET DATA
+PD=subset(EE,HOSTS=="POTATO")
+attach(PD)
+# CREATE FACTORS FOR EACH FACTOR
+PISOLATE=factor(PD$ISOLATES,levels=c("4","1","2","3"))
+
+AUSPC=ggplot(data=PD,aes(x=PISOLATE,y=PD$SENESCENCE,group=PISOLATE,colour=PD$HOSTS)) +
+  geom_boxplot(outlier.size=0,alpha=1/2,lwd=0.72,notch=F)+
+  geom_jitter(aes(fill=PD$HOSTS),size=8,pch=20,alpha = 1/3,position=position_jitter(w=0.05,h=0.01))+
+  scale_colour_manual(values = c("black"))+
+  theme_bw()+theme(panel.grid.major=element_line(colour="white"))+
+  labs(x="Hosts",y="Area under disease progress curve (AUSPC)")+
+  theme(axis.title.x = element_text(size=18,face="bold"))+
+  theme(axis.title.y = element_text(size=18,face="bold"))+
+  scale_x_discrete(labels=c("Control","653",
+                            "111","461"))+
+  theme(axis.title.x = element_text(size=10))+
+  theme(axis.title.y = element_text(size=10))+
+  scale_y_continuous(limits=c(0,4000))+
+  theme(axis.text.x=element_text(angle=1,size=10,face="bold"))+
+  theme(axis.text.y=element_text(size=10,face="bold"))+
+  theme(legend.position="none")
+tiff("AUSPC.tiff",width=8,height=5,units='in',res=600) 
+AUSPC
+dev.off()
+# HISTOGRAM FOR AUSPC
+tiff("AUSPC_HISTOGRAM.tiff",width=10,height=5,units='in',res=600) 
+hist(PD$SENESCENCE,breaks=30,col="grey")
+dev.off()
+#-----------------------------------------------------------------------#
+### HYPOTHESIS TESTING
+## BUILD MODEL FOR YIELDS
+# POTATO
+PD=subset(EE,HOSTS=="POTATO")
+YM=lm(YIELDS~ISOLATES*TRIALS+BLOCKS,data=PD)
+anova(YM)
+plot(YM)
+# MUSTARDS
+MD=subset(EE,HOSTS=="MUSTARD")
+YM=lm(YIELDS~ISOLATES*TRIALS+BLOCKS,data=MD)
+anova(YM)
+plot(YM)
+# BARLEY
+BD=subset(EE,HOSTS=="BARLEY")
+YM=lm(YIELDS~ISOLATES*TRIALS+BLOCKS,data=BD)
+anova(YM)
+plot(YM)
+## BUILD MODEL FOR SENESCENCE
+# POTATO
+PD=subset(EE,HOSTS=="POTATO")
+SM=lm(SENESCENCE~ISOLATES*TRIALS+BLOCKS,data=PD)
+anova(SM)
+plot(SM)
+# Analysis of Variance Table
+Response: SENESCENCE
+Df  Sum Sq Mean Sq F value    Pr(>F)    
+ISOLATES         3 5572735 1857578 10.5992 1.664e-05 ***
+TRIALS           1  433001  433001  2.4707   0.12230    
+BLOCKS           9 3447669  383074  2.1858   0.03884 *  
+ISOLATES:TRIALS  3 1604373  534791  3.0515   0.03692 *  
+Residuals       50 8762849  175257   
+# DUNCANS TEST
+# INVOKE PACKAGES
+library(agricolae)
+duncan.test(SM,"ISOLATES",console=T)
+Study: SM ~ "ISOLATES"
+
+# Duncan's new multiple range test
+for SENESCENCE 
+
+Mean Square Error:  175257 
+
+ISOLATES,  means
+
+  SENESCENCE      std  r  Min  Max
+1   2630.147 485.0439 17 1470 3640
+2   2656.719 494.7362 16 1470 3325
+3   2073.235 535.8658 17 1085 3010
+4   2059.853 372.3001 17 1575 2765
+
+alpha: 0.05 ; Df Error: 50 
+
+Critical Range
+       2        3        4 
+290.6562 305.7036 315.5976 
+
+Harmonic Mean of Cell Sizes  16.73846
+
+Different value for each comparison
+Means with the same letter are not significantly different.
+
+Groups, Treatments and means
+a 	 2 	 2657 
+a 	 1 	 2630 
+b 	 3 	 2073 
+b 	 4 	 2060 
